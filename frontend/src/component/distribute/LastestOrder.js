2a67849bbf77d9050651d7326b4f7c51a9096b7a
Luis Ngo <ngo_l@worksap.co.jp>
2019-09-20 12:49:49 +0800

    generate orderlist

--- frontend/src/component/distribute/LastestOrder.js
+++ frontend/src/component/distribute/LastestOrder.js
@@ -6,169 +6,180 @@ import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/styles';
 import theme from "../../theme";
 import {
-    Card,
-    CardActions,
-    CardHeader,
-    CardContent,
-    Button,
-    Divider,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-    Tooltip,
-    TableSortLabel
+  Card,
+  CardActions,
+  CardHeader,
+  CardContent,
+  Button,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  TableSortLabel
 } from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 import mockData from './data';
 import StatusBullet from "./StatusBullet";
 import {authentication} from "../Authentication";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 
 const useStyles = makeStyles(theme => ({
-    root: {},
-    content: {
-        padding: 0
-    },
-    inner: {
-        minWidth: 800
-    },
-    statusContainer: {
-        display: 'flex',
-        alignItems: 'center'
-    },
-    status: {
-        marginRight: theme.spacing(1)
-    },
-    actions: {
-        justifyContent: 'flex-end'
-    },
-    paper: {},
+  root: {},
+  content: {
+    padding: 0
+  },
+  inner: {
+    minWidth: 800
+  },
+  statusContainer: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  status: {
+    marginRight: theme.spacing(1)
+  },
+  actions: {
+    justifyContent: 'flex-end'
+  },
+  paper: {},
 }));
 
 const statusColors = {
-    delivered: 'success',
-    pending: 'info',
-    refunded: 'danger'
+  delivered: 'success',
+  pending: 'info',
+  refunded: 'danger'
 };
 
 const LatestOrders = props => {
-    const {className, ...rest} = props;
+  const {className, ...rest} = props;
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [orders] = useState(mockData);
-    const [auth, setAuth] = React.useState(undefined);
-    authentication("auth/order", onChange);
-    if (auth === undefined){
-        return blank();
-    }else if (auth) {
-        return generatePage()
-    } else {
-        return fail()
-    }
-    function onChange(auth) {
-        setAuth(auth);
-    }
+  const [orders] = useState(mockData);
+  // const [auth, setAuth] = React.useState(undefined);
 
-    function fail() {
-        return (<div>NOT PERMITTED</div>)
-    }
+  function getData(callback) {
+    let token = Cookies.get('access_token');
+    token = ("Bearer " + token);
 
-    function blank() {
-        return (<div/>)
-    }
-    function generatePage() {
-        return (
-          <Card
-            {...rest}
-            className={clsx(classes.root, className)}
-          >
-              <CardHeader
-                action={
-                    <Button
-                      color="primary"
-                      size="small"
-                      variant="outlined"
+    let URL = "http://localhost:8080/order/getList";
+    axios.defaults.headers.common['Authorization'] = token;
+    axios.get(URL, {}).then(r => {
+      if (r.status == 200) {
+        callback(r.data.order);
+      } else {
+        callback(undefined);
+      }
+    }).catch(e => {
+      callback(undefined);
+    });
+
+  };
+  const [data, setData] = React.useState(undefined);
+
+  if (data === undefined) {
+    getData((a) => onChange(a));
+    return blank();
+  } else {
+    return generatePage()
+  }
+
+  function onChange(data) {
+    setData(data);
+  }
+
+  function blank() {
+    return (<div/>)
+  }
+
+  function viewOrderDetail(orderId) {
+    props.history.push('/orderDetail?orderId=' + orderId);
+
+  }
+
+  function generatePage() {
+    return (
+      <Card
+        {...rest}
+        className={clsx(classes.root, className)}
+      >
+        <CardHeader
+          title="List Orders"
+        />
+        <Divider/>
+        <CardContent className={classes.content}>
+          <PerfectScrollbar>
+            <div className={classes.inner}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Order Ref</TableCell>
+                    <TableCell>Order Link</TableCell>
+                    <TableCell>
+                      <Tooltip
+                        enterDelay={300}
+                        title="Sort"
+                      >
+                        <TableSortLabel
+                          active
+                          direction="desc"
+                        >
+                          Order Date Time
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                    <TableCell>Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.map(order => (
+                    <TableRow
+                      hover
+                      key={order.id}
+                      onClick={()=>viewOrderDetail(order.id)}
+                      style={{cursor:"pointer"}}
                     >
-                        New entry
-                    </Button>
-                }
-                title="Latest Orders"
-              />
-              <Divider/>
-              <CardContent className={classes.content}>
-                  <PerfectScrollbar>
-                      <div className={classes.inner}>
-                          <Table>
-                              <TableHead>
-                                  <TableRow>
-                                      <TableCell>Order Ref</TableCell>
-                                      <TableCell>Customer</TableCell>
-                                      <TableCell sortDirection="desc">
-                                          <Tooltip
-                                            enterDelay={300}
-                                            title="Sort"
-                                          >
-                                              <TableSortLabel
-                                                active
-                                                direction="desc"
-                                              >
-                                                  Date
-                                              </TableSortLabel>
-                                          </Tooltip>
-                                      </TableCell>
-                                      <TableCell>Status</TableCell>
-                                  </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                  {orders.map(order => (
-                                    <TableRow
-                                      hover
-                                      key={order.id}
-                                    >
-                                        <TableCell>{order.ref}</TableCell>
-                                        <TableCell>{order.customer.name}</TableCell>
-                                        <TableCell>
-                                            {moment(order.createdAt).format('DD/MM/YYYY')}
-                                        </TableCell>
-                                        <TableCell>
-                                            <div className={classes.statusContainer}>
-                                                <StatusBullet
-                                                  className={classes.status}
-                                                  color={statusColors[order.status]}
-                                                  size="sm"
-                                                />
-                                                {order.status}
-                                            </div>
-                                        </TableCell>
-                                    </TableRow>
-                                  ))}
-                              </TableBody>
-                          </Table>
-                      </div>
-                  </PerfectScrollbar>
-              </CardContent>
-              <Divider/>
-              <CardActions className={classes.actions}>
-                  <Button
-                    color="primary"
-                    size="small"
-                    variant="text"
-                  >
-                      View all <ArrowRightIcon/>
-                  </Button>
-              </CardActions>
-          </Card>
-        );
-    }
+                      <TableCell>{order.id}</TableCell>
+                      <TableCell>{order.orderLink}</TableCell>
+                      <TableCell>
+                        {moment(order.orderDateTime).format('DD/MM/YYYY')}
+                      </TableCell>
+                      <TableCell>
+                        <div className={classes.statusContainer}>
+                          {order.status}
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </PerfectScrollbar>
+        </CardContent>
+        <Divider/>
+        <CardActions className={classes.actions}>
+          <Button
+            color="primary"
+            size="small"
+            variant="text"
+          >
+            View all <ArrowRightIcon/>
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 
 };
 
 LatestOrders.propTypes = {
-    className: PropTypes.string
+  className: PropTypes.string
 };
 
 export default LatestOrders;
